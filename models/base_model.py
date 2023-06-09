4e614edada0fa816a8503970b73c3471f90faa06
root <benjaminitonio@gmail.com>
2023-06-09 11:48:57 -0700

    task 4 attempt 1, addind args and kwargs to init

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,20 @@ from datetime import datetime
 
 class BaseModel():
     """Base Model Class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """Initialization of BaseModel class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+            
+
 
     def save(self):
         """Saves and updates the date"""
