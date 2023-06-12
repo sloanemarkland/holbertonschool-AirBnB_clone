a28e41aff1eef79d96e99a8b2a33e23b880acb06
root <benjaminitonio@gmail.com>
2023-06-12 13:53:47 -0700

    ben user attempt 2

--- models/user.py
+++ models/user.py
@@ -7,9 +7,11 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class inherits from BaseModel"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
     def __init__(self):
-        super().__init__()
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+        """initialization of User class"""
+        super().__init__(*args, **kwargs)
