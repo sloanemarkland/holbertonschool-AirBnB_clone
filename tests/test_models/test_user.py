144fed1228d11a0c2c81633b3f068a3d1e0546c9
sarahmarkland <sarah.markland@icloud.com>
2023-06-13 17:51:18 -0500

    did it for mariah

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,8 +4,9 @@ from models.user import User
 
 class TestUser(unittest.TestCase):
     """TestUser class documentation"""
-
-
+    def setUp(self):
+        """ setUp method for User class """
+        xxx
 
 if __name__ == '__main__':
     unittest.main()
