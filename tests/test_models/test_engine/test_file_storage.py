202a8329e3712bf1049fa25cac1755926fb1ce4b
sarahmarkland <sarah.markland@icloud.com>
2023-06-13 14:34:01 -0500

    couple of file_storage tests

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,39 @@
+"""test_file_storage module"""
+import unittest
+import os
+import json
+from models import storage
+from models.engine import file_storage
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+class TestFileStorage(unittest.TestCase):
+    """TestFileStorage class documentation"""
+    def setUp(self):
+        """ setup for all tests """
+        self.model = BaseModel()
+        self.storage = FileStorage()
+        self.storage = save()
+
+    def tearDown(self):
+        """ teardown for all tests """
+        del self.model
+        del self.storage
+        os.remove("file.json")
+
+    def test_new(self):
+        """ test file_storage new method """
+        model = BaseModel()
+        self.storage.new(model)
+        my_dict = self.file_storage.all()
+        self.assertIn(model, my_dict.values())
+    
+    def test_all(self):
+        """ test file_storage all method """
+        model = BaseModel()
+        my_dict = self.storage.all()
+        self.assertIsInstance(my_dict, dict)
+        self.assertIn(model, my_dict.values())
+
+if __name__ == '__main__':
+    unittest.main()
